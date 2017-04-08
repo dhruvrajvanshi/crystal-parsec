51b0ffca3e7b0db02142c96bf1d7cfd363116ef5
Dhruv Rajvanshi <dhruv.rajvanshi@acetone.co>
2017-04-08 14:02:29 +0530

    Finished JSON example

---
+++ examples/json.cr
@@ -0,0 +1,158 @@
+require "crz"
+require "../src/parsec"
+include Parsec
+
+## JSON syntax tree.
+## Uses CRZ algebraic data types for brevity
+## This translates into a simple class hierarchy with JSON
+## as an abstract base class and JNumber, JString, ..., etc
+## as derived concrete classes containing values of types
+## mentioned in the parens.
+adt JSON,
+  JNumber(Float64),
+  JString(String),
+  JBool(Bool),
+  JArray(Array(JSON)),
+  JObject(Hash(String, JSON)),
+  JNull
+
+alias JNumber = JSON::JNumber
+alias JString = JSON::JString
+alias JBool   = JSON::JBool
+alias JArray  = JSON::JArray
+alias JObject = JSON::JObject
+alias JNull   = JSON::JNull
+
+
+def whitespace
+  many_1(one_of "\n\t ") 
+end
+def optional_whitespace
+  many(one_of "\n\t ")
+end
+def json_p : Parser(JSON)
+  array_p | bool_p | null_p | jstring_p | number_p | object_p
+end
+
+def bool_p 
+  (true_p | false_p).map {|v| JBool.new v }
+      .map {|v| v.as JSON }
+end
+
+def true_p
+  (string "true").map {|_| true }
+end
+def false_p
+  (string "false").map {|_| false }
+end
+
+def null_p
+  string("null") >> Parser.pure(JNull.new.as JSON)
+end
+def comma
+  comma = optional_whitespace >> char(',') >> optional_whitespace
+end
+def array_p
+  mdo({
+    _   <= char('['),
+    arr <= sep_by(json_p.as Parser(JSON), comma),
+    _   <= char(']'),
+    Parser.pure(JArray.new(arr))
+  }).map {|a| a.as JSON }
+end
+
+def string_p
+  mdo({
+    _ <= char('"'),
+    arr <= many(
+        (string("\\\"").map {|x| "\""}) | # escaped quote
+        (string("\\n").map {|x| "\n"}) | # escaped newline
+        (string("\\\\").map {|x| "\\"}) | # escaped backslash
+        (none_of("\"\\").map {|x| x.to_s}) # other characters
+      ),
+    _ <= char('"'),
+    str = (arr.reduce {|prev, current| prev + current}),
+    Parser.pure(str)
+  })
+end
+
+def jstring_p
+  string_p.map {|s| JString.new(s).as(JSON)}
+end
+
+# concatenate elements of array into string
+def concatenate(arr)
+  result = ""
+    arr.each do |char|
+      result += char.to_s
+    end
+  result
+end
+
+def float_p
+  mdo({
+    before <= (many_1(digit).map {|a| concatenate(a) }),
+    _ <= char('.'),
+    after <= (many_1(digit).map {|a| concatenate(a)}),
+    num = (before + "." + after).to_f,
+    json = (JNumber.new num).as(JSON),
+    Parser.pure(json)
+  })
+end
+
+def int_p
+  many_1(digit).map {|a| concatenate(a)}
+    .map(&.to_f)
+    .map {|x| JNumber.new(x).as(JSON) }
+end
+
+def number_p
+  int_p | float_p
+end
+
+def object_pair
+  mdo({
+    key <= string_p,
+    _ <= optional_whitespace,
+    _ <= char(':'),
+    _ <= optional_whitespace,
+    value <= json_p.as(Parser(JSON)),
+    Parser.pure({key, value})
+  })
+end
+
+def create_object(arr : Array({String, JSON})) : Parser(JSON)
+  map = {} of String => JSON
+  arr.each do |tuple|
+    if map[tuple[0]]?
+      return Parsec.fail(JSON, "Duplicate key \"#{tuple[0]}\" in object")
+    else
+      map[tuple[0]] = tuple[1]
+    end
+  end
+  Parser.pure((JObject.new map).as(JSON))
+end
+
+def object_p
+  mdo({
+    _ <= (char('{') >> optional_whitespace),
+    pairs <= sep_by(object_pair, comma),
+    _ <= (optional_whitespace >> char('}')),
+    create_object(pairs)
+  })
+end
+
+
+# pp array_p.parse("[true , false, null, [null, true]]")
+pp json_p.parse("\"a\\\\\\nsdf\"")
+pp int_p.parse("123.234")
+
+pp object_pair.parse("\"key\"  :  23")
+
+pp object_p.parse("{
+    \"key\": 23,
+    \"key1\": {
+      \"k\": [\"asdf\", 2, true, {}]
+    }
+  }
+")
--- src/parsec.cr
+++ src/parsec.cr
@@ -1,6 +1,7 @@
 require "crz"
 
 module Parsec
+  include CRZ::Monad::Macros
   extend self
   class ParseState
     getter input
@@ -147,9 +148,6 @@ module Parsec
         result = f.call state
         count+=1
       end
-      puts count
-      puts acc.to_s
-      puts result.value0.to_s
       ParseResult::Success.new acc, state
     end
   end
@@ -172,132 +170,24 @@ module Parsec
     (p.map {|x| [x]}) |
     Parser.pure([] of T)
   end
+
+  def lowercase
+    one_of "qwertyuiopasdfghjklzxcvbnm"
+  end
+
+  def uppercase 
+    one_of "QWERTYUIOPASDFGHJKLZXCVBNM"
+  end
+
+  def alphabet
+    lowercase | uppercase
+  end
+
+  def digit
+    one_of "1234567890"
+  end
+
+  def alphanum
+    digit | alphabet
+  end
 end
-
-include Parsec
-
-p = Parser(Int32).new do |state|
-  ParseResult::Error(Int32).new "error"
-end
-
-puts (p.parse "asdf")
-
-p3 = Parser.pure 3
-
-puts (p3.parse "234")
-
-cP = char 'c'
-
-puts cP.parse("cd")
-
-puts string("asfgd").parse("asf").to_s
-
-puts fail(Int32, "Unknown").parse("asdf").to_s
-
-f = (Parser.pure 13).map do |x|
-  x + 2
-end
-
-
-puts f.parse("asdf").to_s
-
-f2 = (string "asdf") >> (string "12")
-puts f2.parse("12asdf").to_s
-
-p4 = Parser.pure(3) >= (->(x : Int32) { Parser.pure(x + 1) })
-
-puts p4.parse("asdf")
-
-
-p = (string "asdf") | (string "qwer")
-
-puts p.parse("qwder").to_s
-
-puts (one_of "asdf").parse("4").to_s
-
-
-p = many (string "asdf")
-puts p.parse("asdfasdfasdfasdfaa").to_s
-
-p = many_1 (string "asdf")
-puts p.parse("").to_s
-
-p = none_of "asdf"
-puts p.parse("a").to_s # error
-puts p.parse("q").to_s # q
-
-
-
-## JSON
-
-adt JSON,
-  JNumber(Float64),
-  JString(String),
-  JBool(Bool),
-  JArray(Array(JSON)),
-  JObject(Hash(String, JSON)),
-  JNull
-
-alias JNumber = JSON::JNumber
-alias JString = JSON::JString
-alias JBool   = JSON::JBool
-alias JArray  = JSON::JArray
-alias JObject = JSON::JObject
-alias JNull   = JSON::JNull
-
-
-def whitespace
-  many_1(one_of "\n\t ") 
-end
-def optional_whitespace
-  many(one_of "\n\t ")
-end
-def json_p : Parser(JSON)
-  array_p | bool_p | null_p
-end
-
-def bool_p 
-  (true_p | false_p).map {|v| JBool.new v }
-      .map {|v| v.as JSON }
-end
-
-def true_p
-  (string "true").map {|_| true }
-end
-def false_p
-  (string "false").map {|_| false }
-end
-
-def null_p
-  string("null") >> Parser.pure(JNull.new.as JSON)
-end
-comma = optional_whitespace >> char(',') >> optional_whitespace
-
-def array_p
-  mdo({
-    _   <= char('['),
-    arr <= sep_by(json_p.as Parser(JSON), comma),
-    _   <= char(']'),
-    Parser.pure(JArray.new(arr))
-  }).map {|a| a.as JSON }
-end
-
-def string_p
-  string_char = (string("\\\"").map {|x| "\""}) | # escaped quote
-        (string("\\n").map {|x| "\n"}) | # escaped newline
-        (string("\\\\").map {|x| "\\"}) | # escaped backslash
-        (none_of("\"\\").map {|x| x.to_s}) # other characters
-  mdo({
-    _ <= char('"'),
-    arr <= many(
-        string_char
-      ),
-    _ <= char('"'),
-    x = "asdf",
-    Parser.pure(arr.reduce {|prev, current| prev + current})
-  })
-end
-
-
-# pp array_p.parse("[true , false, null, [null, true]]")
-pp string_p.parse("\"a\\\\\\nsdf\"")
